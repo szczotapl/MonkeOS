0e7171de48a733438ac61c0fe3113a6218a14a7c
Blackneeeed <dodatkowekonto.marcelclay@gmail.com>
2024-03-17 19:02:30 +0100

    ...

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,121 +1,18 @@
-#include <cstddef>
 #include <cstdint>
+#include <cstddef>
 #include <cstring>
-
-enum class VGAColor : uint8_t {
-    Black = 0,
-    Blue = 1,
-    Green = 2,
-    Cyan = 3,
-    Red = 4,
-    Magenta = 5,
-    Brown = 6,
-    LightGrey = 7,
-    DarkGrey = 8,
-    LightBlue = 9,
-    LightGreen = 10,
-    LightCyan = 11,
-    LightRed = 12,
-    LightMagenta = 13,
-    LightBrown = 14,
-    White = 15
-};
-
-uint8_t makeColor(VGAColor fg, VGAColor bg) {
-    return static_cast<uint8_t>(fg) | static_cast<uint8_t>(bg) << 4;
-}
-
-uint16_t makeVGAEntry(char c, uint8_t color) {
-    uint16_t c16 = c;
-    uint16_t color16 = color;
-    return c16 | color16 << 8;
-}
-
-constexpr size_t VGA_WIDTH = 80;
-constexpr size_t VGA_HEIGHT = 25;
-
-size_t termRow;
-size_t termColumn;
-uint8_t termColor;
-uint16_t* termBuffer;
-
-void termInitialize() {
-    termRow = 0;
-    termColumn = 0;
-    termColor = makeColor(VGAColor::LightGrey, VGAColor::Black);
-    termBuffer = reinterpret_cast<uint16_t*>(0xB8000);
-    for (size_t y = 0; y < VGA_HEIGHT; y++) {
-        for (size_t x = 0; x < VGA_WIDTH; x++) {
-            const size_t index = y * VGA_WIDTH + x;
-            termBuffer[index] = makeVGAEntry(' ', termColor);
-        }
-    }
-}
-
-void termPutChar(char c) {
-    if (c == '\n') {
-        termColumn = 0;
-        if (++termRow == VGA_HEIGHT) {
-            for (size_t y = 1; y < VGA_HEIGHT; y++) {
-                for (size_t x = 0; x < VGA_WIDTH; x++) {
-                    const size_t destIndex = (y - 1) * VGA_WIDTH + x;
-                    const size_t srcIndex = y * VGA_WIDTH + x;
-                    termBuffer[destIndex] = termBuffer[srcIndex];
-                }
-            }
-            for (size_t x = 0; x < VGA_WIDTH; x++) {
-                const size_t index = (VGA_HEIGHT - 1) * VGA_WIDTH + x;
-                termBuffer[index] = makeVGAEntry(' ', termColor);
-            }
-            termRow = VGA_HEIGHT - 1;
-        }
-    } else {
-        const size_t index = termRow * VGA_WIDTH + termColumn;
-        termBuffer[index] = makeVGAEntry(c, termColor);
-        if (++termColumn == VGA_WIDTH) {
-            termColumn = 0;
-            if (++termRow == VGA_HEIGHT) {
-                    termRow = 0;
-                }
-            }
-    }
-}
-
-void write(const char* data) {
-    for (size_t i = 0; data[i] != '\0'; i++)
-        termPutChar(data[i]);
-}
-
-void writeLine(const char* data) {
-    write(data);
-    write("\n");
-}
-
-void writeError(const char* message) {
-    write("[ Error ] ");
-    writeLine(message);
-}
-
-void writeInfo(const char* message) {
-    write("[ Info ] ");
-    writeLine(message);
-}
-
-void writeWarn(const char* message) {
-    write("[ Warning ] ");
-    writeLine(message);
-}
+#include "term.cpp"
 
 extern "C" void kernel_main() {
     termInitialize();
-    writeLine("         .-\"-.");
-    writeLine("       _/.-.-.\\_");
-    writeLine("      ( ( o o ) )");
-    writeLine("       |/  \"  \\|    Monke Stronk");
-    writeLine("        \\'/^'/'");
-    writeLine("        /`\\ /`\\");
-    writeLine("       /  /|\\  \\");
-    writeLine("      ( (/ T \\) )");
-    writeLine("         \\__/^\\__/");
-    writeInfo("Booted MonkeOS!");
+    termWriteLine("         .-\"-.");
+    termWriteLine("       _/.-.-.\\_");
+    termWriteLine("      ( ( o o ) )");
+    termWriteLine("       |/  \"  \\|    Monke Stronk");
+    termWriteLine("        \\'/^'/'");
+    termWriteLine("        /`\\ /`\\");
+    termWriteLine("       /  /|\\  \\");
+    termWriteLine("      ( (/ T \\) )");
+    termWriteLine("         \\__/^\\__/");
+    termWriteInfo("Booted MonkeOS!");
 }
---
+++ src/term.cpp
@@ -0,0 +1,106 @@
+#pragma once
+
+#include <cstdint>
+#include <cstddef>
+#include <cstring>
+
+enum class VGAColor : uint8_t {
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGrey = 7,
+    DarkGrey = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    LightMagenta = 13,
+    LightBrown = 14,
+    White = 15
+};
+
+uint8_t makeColor(VGAColor fg, VGAColor bg) {
+    return static_cast<uint8_t>(fg) | static_cast<uint8_t>(bg) << 4;
+}
+
+uint16_t makeVGAEntry(char c, uint8_t color) {
+    uint16_t c16 = c;
+    uint16_t color16 = color;
+    return c16 | color16 << 8;
+}
+
+const size_t VGA_COLS = 80;
+const size_t VGA_ROWS = 25;
+
+size_t termRow = 0;
+size_t termColumn = 0;
+uint8_t defaultColor = makeColor(VGAColor::LightGrey, VGAColor::Black);
+uint16_t* termBuffer = (uint16_t*)0xb8000;
+
+void termClearScreen(uint8_t color = defaultColor) {
+    for (uint16_t pos = 0; pos < VGA_ROWS * VGA_COLS; pos++) {
+        termBuffer[pos] = makeVGAEntry(' ', color);
+    }
+}
+
+void termInitialize() {
+    termClearScreen(defaultColor);
+}
+
+void termPutChar(char c, uint8_t color = defaultColor) {
+    if (c == '\n') {
+        termColumn = 0;
+        if (++termRow == VGA_ROWS) {
+            for (size_t y = 1; y < VGA_ROWS; y++) {
+                for (size_t x = 0; x < VGA_COLS; x++) {
+                    const size_t destIndex = (y - 1) * VGA_COLS + x;
+                    const size_t srcIndex = y * VGA_COLS + x;
+                    termBuffer[destIndex] = termBuffer[srcIndex];
+                }
+            }
+            for (size_t x = 0; x < VGA_COLS; x++) {
+                const size_t index = (VGA_ROWS - 1) * VGA_COLS + x;
+                termBuffer[index] = makeVGAEntry(' ', color);
+            }
+            termRow = VGA_ROWS - 1;
+        }
+    } else {
+        const size_t index = termRow * VGA_COLS + termColumn;
+        termBuffer[index] = makeVGAEntry(c, color);
+        if (++termColumn == VGA_COLS) {
+            termColumn = 0;
+            if (++termRow == VGA_ROWS) {
+                    termRow = 0;
+                }
+            }
+    }
+}
+
+void termWrite(const char* data, uint8_t color = defaultColor) {
+    for (size_t i = 0; data[i] != '\0'; i++)
+        termPutChar(data[i], color);
+}
+
+void termWriteLine(const char* data, uint8_t color = defaultColor) {
+    termWrite(data, color);
+    termWrite("\n");
+}
+
+void termWriteError(const char* message, uint8_t color = defaultColor) {
+    termWrite("[ERR] ", makeColor(VGAColor::Red, VGAColor::Black));
+    termWriteLine(message, color);
+}
+
+void termWriteInfo(const char* message, uint8_t color = defaultColor) {
+    termWrite("[INFO] ", makeColor(VGAColor::LightCyan, VGAColor::Black));
+    termWriteLine(message, color);
+}
+
+void termWriteWarn(const char* message, uint8_t color = defaultColor) {
+    termWrite("[WARN] ", makeColor(VGAColor::LightBrown, VGAColor::Black));
+    termWriteLine(message, color);
+}
